01358a7a333404030b00e5ede6bb1a3dd07f4bdc
zhanghuahan <928288106@qq.com>
2020-01-15 09:05:33 +0800

    extract github config from to property

---
+++ src/main/java/life/majiang/community/dto/GithubUser.java
@@ -0,0 +1,32 @@
+package life.majiang.community.dto;
+
+public class GithubUser {
+    private String name;
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    private  String bio;
+}
